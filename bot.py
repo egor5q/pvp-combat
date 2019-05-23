42d0f9df14e35c408deb7a360a9f7544ceae7dd7
egor5q <31898567+egor5q@users.noreply.github.com>
2019-05-23 17:19:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,11 +13,42 @@ token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
 
 
-client=MongoClient(os.environ['database'])
-db=client.
-users=db.users
+#client=MongoClient(os.environ['database'])
+#db=client.
+#users=db.users
 
 
+
+
+
+def battle(game):
+    if game['started']==False:
+        game['started']=True
+        for ids in game['players']:
+            sendkb(game['players'][ids], game)
+
+
+def sendkb(player, game):
+    if player['die']!=1 and player['stun']<=0:
+        kb=types.ReplyKeyboardMarkup()
+        kb.add(types.KeyboardButton('Атаковать'), types.KeyboardButton('Блокировать'))
+        gamestats=stats(game)
+        bot.send_message(player['id'], gamestats, reply_markup=kb)
+            
+  
+              
+def createplayer(user):
+    return {user.id:{
+        'hp':1000,
+        'stamina':15,
+        'stun':0,
+        'status':'rest',
+        'action':None
+    }
+           }
+              
+              
+            
 def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode=None):
     return bot.edit_message_text(chat_id=chat_id,message_id=message_id,text=message_text,reply_markup=reply_markup,
                                  parse_mode=parse_mode)   
